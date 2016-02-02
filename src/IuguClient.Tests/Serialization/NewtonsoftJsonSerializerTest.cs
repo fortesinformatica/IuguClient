fb487c3686978eb8a716fda1684a369ee7acada2
Alberto Monteiro <alberto.monteiro@live.com>
2016-02-02 10:05:53 -0300

    100% coverage again

---
+++ src/IuguClient.Tests/Serialization/NewtonsoftJsonSerializerTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace IuguClientAPI.Tests.Serialization
+{
+    [TestFixture]
+    public class NewtonsoftJsonSerializerTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var jsonSerializer = NewtonsoftJsonSerializer.Instance;
+            jsonSerializer.DateFormat = "dd/MM/yyyy";
+            jsonSerializer.Namespace = "empty";
+            jsonSerializer.RootElement = "empty";
+            Assert.AreEqual("dd/MM/yyyy", jsonSerializer.DateFormat);
+            Assert.AreEqual("empty", jsonSerializer.Namespace);
+            Assert.AreEqual("empty", jsonSerializer.RootElement);
+            Assert.AreEqual("application/json", jsonSerializer.ContentType);
+        }
+    }
+}
